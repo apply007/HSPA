203e7dfe8e098f59d9b66c384f3414d4b8ead62b
apply007 <hasanat153@gmail.com>
2023-08-05 22:18:59 +0600

    pproperty list parent to property card child data send

--- src/app/Property/property-list/property-list.component.ts
+++ src/app/Property/property-list/property-list.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent {
-
+  Properties: Array<any> = [
+    {
+      Id: 1,
+      Name: 'Ashif House',
+      Type: 'House',
+      Price: 666678,
+    },
+    {
+      Id: 2,
+      Name: `Hasibul's House`,
+      Type: 'House',
+      Price: 89890,
+    },
+    {
+      Id: 3,
+      Name: 'Great House',
+      Type: 'House',
+      Price: 3425345,
+    },
+    {
+      Id: 4,
+      Name: 'Hasan House',
+      Type: 'Duplex',
+      Price: 3456700,
+    },
+    {
+      Id: 5,
+      Name: ' House',
+      Type: 'Large',
+      Price: 56000,
+    },
+  ];
 }
